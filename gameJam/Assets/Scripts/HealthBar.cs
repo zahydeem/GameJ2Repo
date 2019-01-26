5de029ab4bdb2f5a8029d0d7c2cb31fde2ac2ac4
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 17:39:00 -0600

    HealthFinished

--- gameJam/Assets/Scripts/HealthBar.cs
+++ gameJam/Assets/Scripts/HealthBar.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    GenericCreature playerCreatureScript;
+    Image healthImage;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCreatureScript = GameController.gameController.player.GetComponent<GenericCreature>();
+        healthImage = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthImage.fillAmount = playerCreatureScript.currentHealth / playerCreatureScript.maxHealth;
     }
 }
