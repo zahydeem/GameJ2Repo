3f5aeb83c03d623d9b9b721ba6683f8f2689efad
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 17:18:23 -0600

    Creature

---
+++ gameJam/Assets/Scripts/AbstractCreature.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractCreature : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void private RefreshHealth()
+    {
+        currentHealth = maxHealth();
+    }
+}
