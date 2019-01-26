49b4ea6eefc379544351f51a2fee8c750bf84ffd
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 16:26:24 -0600

    mystuff

---
+++ gameJam/Assets/Scripts/AbstractCreature.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractCreature : MonoBehaviour
+{
+    public float maxHealth;
+
+    public float currentHealth;
+
+    private void Start()
+    {
+        RefreshHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RefreshHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
---
+++ gameJam/Assets/Scripts/ToolBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBar : MonoBehaviour
+{
+    public GameObject cellPrefab;
+
+    private float numCells;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numCells = GameController.gameController.toolBarSize;
+        Populate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Populate()
+    {
+        for (int i = 0; i < numCells; i++)
+        {
+            CreateCell();
+        }
+    }
+
+    private void CreateCell()
+    {
+        GameObject cell;
+        cell = Instantiate(cellPrefab, transform);
+    }
+}
