facd999db284c6fcefa01dc2823a27e1476836d9
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 17:25:57 -0600

    HealthBar

---
+++ gameJam/Assets/Scripts/GenericCreature.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericCreature : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RefreshHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
--- gameJam/Assets/Scripts/HealthBar.cs
+++ gameJam/Assets/Scripts/HealthBar.cs
@@ -2,25 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbstractCreature : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
-    public int maxHealth;
-    public int currentHealth;
-
     // Start is called before the first frame update
     void Start()
     {
-        RefreshHealth();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void private RefreshHealth()
-    {
-        currentHealth = maxHealth();
     }
 }
