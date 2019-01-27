3fcce756849a5d03c46db35bf8f717f908b5aefe
zahydeem <44513031+zahydeem@users.noreply.github.com>
2019-01-26 18:38:43 -0600

    enemy

--- gameJam/Assets/Scripts/ChangeEnemy.cs
+++ gameJam/Assets/Scripts/ChangeEnemy.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ChangeEnemy : MonoBehaviour
 {
-    /*
+    
     public Sprite D1, D2, D3;
-    i
+    int tick = 0;
+    public float speed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +18,29 @@ public class ChangeEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        St
-        
-        this.GetComponent<SpriteRenderer>().sprite = D1;
-        this.GetComponent<SpriteRenderer>().sprite = D1;
-        this.GetComponent<SpriteRenderer>().sprite = D1;
+
+        StartCoroutine(Next());
+        if (tick == 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = D1;
+        }
+        if (tick == 1)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = D2;
+        }
+        if (tick == 2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = D3;
+            tick = 0;
+        }
 
     }
 
     IEnumerator Next()
     {
-
+        tick++;
+        yield return new WaitForSecondsRealtime(speed);
+        
     }
-    */
+    
 }
