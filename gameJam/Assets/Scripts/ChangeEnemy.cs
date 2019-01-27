a54e2f2929f9c0ad20aa70fa50de3657a2ece4b5
zahydeem <44513031+zahydeem@users.noreply.github.com>
2019-01-26 18:52:30 -0600

    h

--- gameJam/Assets/Scripts/ChangeEnemy.cs
+++ gameJam/Assets/Scripts/ChangeEnemy.cs
@@ -7,7 +7,8 @@ public class ChangeEnemy : MonoBehaviour
     
     public Sprite D1, D2, D3;
     int tick = 0;
-    public float speed = 0.5f;
+    public float speed;
+    bool isChanging = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,20 +19,28 @@ public class ChangeEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isChanging == false) {
+            isChanging = true;
+            StartCoroutine(Next());
 
-        StartCoroutine(Next());
-        if (tick == 0)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = D1;
-        }
-        if (tick == 1)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = D2;
-        }
-        if (tick == 2)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = D3;
-            tick = 0;
+            if (tick == 0)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = D1;
+            }
+            else if (tick == 1)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = D2;
+            }
+            else if (tick == 2)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = D3;
+      
+            }
+            else if (tick == 3)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = D2;
+                tick = -1;
+            }
         }
 
     }
@@ -39,7 +48,8 @@ public class ChangeEnemy : MonoBehaviour
     IEnumerator Next()
     {
         tick++;
-        yield return new WaitForSecondsRealtime(speed);
+        yield return new WaitForSeconds(speed);
+        isChanging = false;
         
     }
     
