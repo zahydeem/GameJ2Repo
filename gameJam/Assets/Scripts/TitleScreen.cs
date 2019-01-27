eeff3cc2f20a07497074a2505bc1c2f07bfbc97b
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-27 17:11:51 -0600

    Title

---
+++ gameJam/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void EnterGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneAt(SceneManager.GetActiveScene().buildIndex + 1).name);
+    }
+}
