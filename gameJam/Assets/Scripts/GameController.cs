3e8aa348dc14d63701e71a64d5e18d17818728ce
zahydeem <44513031+zahydeem@users.noreply.github.com>
2019-01-27 14:22:19 -0600

    no

--- gameJam/Assets/Scripts/GameController.cs
+++ gameJam/Assets/Scripts/GameController.cs
@@ -8,31 +8,25 @@ public class GameController : MonoBehaviour
     public static GameController gameController;
 
     public GameObject player;
+    public GameObject note;
 
     public float toolBarSize;
-    public Text text;
-    public Image popup;
+    
 
     // Start is called before the first frame update
     void Awake()
     {
         gameController = this;
         player = transform.GetChild(0).gameObject;
-
-        popup.enabled = false;
-        text.enabled = false;
-      
+         
+        
+        
      
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && popup.enabled == true)
-        {
-            print("die");
-            popup.enabled = false;
-            text.enabled = false;
-        }
+        
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             GameObject clicked = SelectObject();
@@ -42,24 +36,12 @@ public class GameController : MonoBehaviour
                 if (SelectObject().tag == "Interactable")
                 {
                     player.GetComponent<PlayerMovement>().ReachFor();
-                    
 
                 }
                 else if (SelectObject().tag == "Note")
                 {
-                    popup.enabled = true;
-                    text.enabled = true;
-                    if (SelectObject().name == "Note1")
-                    {
-                        text.text = "MOVE TO THE NEXT ISLAND";
-                    }
-                   else if (SelectObject().name == "Note2")
-                    {
-                        text.text = "Just Give up";
-                    }
-
+                    note.GetComponent<NoteSelect>().Mess();
                 }
-               
                 else
                 {
                     player.GetComponent<PlayerMovement>().Attack();
@@ -67,13 +49,7 @@ public class GameController : MonoBehaviour
             }
         }
     }
-    private void OnMouseDown(Collider2D other)
-    {
-        if(other.tag == "Note")
-        {
-            print("check");
-        }
-    }
+  
     private GameObject SelectObject()
     {
         return Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero).collider.gameObject;
---
+++ gameJam/Assets/Scripts/NoteSelect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NoteSelect : MonoBehaviour
+{
+    public Text text;
+    public Image popup;
+    GameObject note;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+       popup.enabled = false;
+       text.enabled = false;
+        print("nope");
+    }
+    public void Mess()
+    {
+        popup.enabled = true;
+        text.enabled = true;
+
+        if (note.name == "Note1")
+        {
+            text.text = "MOVE TO THE NEXT ISLAND";
+        }
+        else if (note.name == "Note2")
+        {
+            text.text = "Just Give up";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && popup.enabled == true)
+        {
+            print("die");
+            popup.enabled = false;
+            text.enabled = false;
+        }
+        
+        
+
+    }
+}
+
+
+
