38cafeda86df1988dd74fd3f95582ec3c94a94da
zahydeem <44513031+zahydeem@users.noreply.github.com>
2019-01-27 12:06:47 -0600

    Revert "Revert "h""
    This reverts commit ea047046a5b6d390a99d9e28a8c067ef67a6552a.

--- gameJam/Assets/Scripts/PlayerMovement.cs
+++ gameJam/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : AbstractMovement
 {
+    public Sprite frontGhost, backGhost, idleGhost, sideMoveGhost;
+
     float moveSpeed = 4f;
     Vector3 forward, right;
     ContactFilter2D contactFilter;
-    float swingSize = 1f;
+    public float reach = 1f;
+    float swingSpeed = 400f;
+    IEnumerator thisCoroutine;
+    Transform swordTransform;
+    Quaternion startSwordRotation;
+    Vector3 startSwordPosition;
+    bool isAttacking;
 
     enum Dir
     {
@@ -27,12 +35,18 @@ public class PlayerMovement : AbstractMovement
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
         dir = Dir.right;
-
-
+        isAttacking = false;
+        
     }
+
 
     private void FixedUpdate()
     {
+        Collider2D[] colliderList = collidersWithinRange();
+        foreach (Collider2D collider in colliderList)
+        {
+            Debug.Log("Collider witin Range Name:" + collider.name);
+        }
         Vector3 copyOfPosition = transform.position;
         float adjustedMoveSpeed = moveSpeed;
         if (Input.GetAxis("HorizontalKey") != 0 && Input.GetAxis("VerticalKey") != 0)
@@ -48,27 +62,38 @@ public class PlayerMovement : AbstractMovement
         {
             transform.position = copyOfPosition;
         }
-        Flip();
-        FlipHor();
+        if (!isAttacking)
+        {
+            Flip();
+            FlipHor();
+        }
     }
 
     private void Flip()
     {
-        if (Input.GetAxis("VerticalKey") == 1 && dir != Dir.up)
+        if (Input.GetAxis("VerticalKey") == 1)
         {
             dir = Dir.up;
+            this.GetComponent<SpriteRenderer>().sprite = backGhost;
         }
-        else if (Input.GetAxis("HorizontalKey") == -1 && dir != Dir.left)
+        else if (Input.GetAxis("HorizontalKey") == -1)
         {
             dir = Dir.left;
+            this.GetComponent<SpriteRenderer>().sprite = sideMoveGhost;
         }
-        else if (Input.GetAxis("HorizontalKey") == 1 && dir != Dir.right)
+        else if (Input.GetAxis("HorizontalKey") == 1)
         {
             dir = Dir.right;
+            this.GetComponent<SpriteRenderer>().sprite = sideMoveGhost;
         }
-        else if (Input.GetAxis("VerticalKey") == -1 && dir != Dir.down)
+        else if (Input.GetAxis("VerticalKey") == -1)
         {
             dir = Dir.down;
+            this.GetComponent<SpriteRenderer>().sprite = frontGhost;
+        }
+        else if ((dir == Dir.left || dir == Dir.right) && Input.GetAxis("HorizontalKey") == 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = idleGhost;
         }
     }
 
@@ -96,9 +121,114 @@ public class PlayerMovement : AbstractMovement
     **/
     public void Attack()
     {
-        Vector2 centerPoint = DetermineSwingArea();
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            SwingSword();
+            Collider2D[] collidersAttack = collidersWithinRange();
+            foreach (Collider2D collider in collidersAttack)
+            {
+                if (collider.tag == "Enemy")
+                {
+                    GetComponent<GenericCreature>().DealDamage(collider.gameObject);
+                }
+            }
+        }
+    }
 
-        Collider2D[] collidersAttack = Physics2D.OverlapBoxAll(centerPoint, new Vector2(swingSize/2, swingSize/2), 0f);
+    void SwingSword()
+    {
+        //transform.GetChild(0).GetComponent<Animator>().SetTrigger("DoSwingSword");
+        swordTransform = transform.GetChild(0).GetChild(0);
+        startSwordPosition = swordTransform.localPosition;
+        swordTransform.localPosition = new Vector3(
+            0f,
+            swordTransform.localPosition.y + 0.25f,
+            swordTransform.localPosition.z
+        );
+        startSwordRotation = swordTransform.localRotation;
+        swordTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        thisCoroutine = SwordSwingRoutine();
+        StartCoroutine(thisCoroutine);
+    }
+
+    void RestoreSword()
+    {
+        swordTransform.localPosition = startSwordPosition;
+        swordTransform.localRotation = startSwordRotation;
+        swordTransform.parent.rotation = Quaternion.Euler(0f, 0f, 0f);
+        isAttacking = false;
+    }
+
+    IEnumerator SwordSwingRoutine()
+    {
+        while (swordTransform.localRotation.eulerAngles.z < 180 - startSwordRotation.z)
+        {
+            if (dir == Dir.up)
+            {
+                swordTransform.parent.localRotation = Quaternion.Euler(0f, 0f, -90f);
+            }
+            else if (dir == Dir.down)
+            {
+                swordTransform.parent.localRotation = Quaternion.Euler(0f, 0f, 90f);
+            }
+            else
+            {
+                swordTransform.parent.localRotation = Quaternion.Euler(0f, 0f, 0f);
+
+            }
+            Vector3 rotationPoint = new Vector3(
+                transform.position.x,
+                transform.position.y - 0.05f,
+                transform.position.z
+            );
+            if (swordTransform.lossyScale.x > 0)
+            {
+                swordTransform.RotateAround(rotationPoint, Vector3.forward, Time.deltaTime * swingSpeed);
+            }
+            else
+            {
+                swordTransform.RotateAround(rotationPoint, Vector3.forward, -Time.deltaTime * swingSpeed);
+            }
+            yield return null;
+        }
+        RestoreSword();
+        StopCoroutine(thisCoroutine);
+        yield return null;
+    }
+
+    public bool isWithinRange(GameObject givenGameObject)
+    {
+        Collider2D[] nearbyColliders = collidersWithinRange();
+        foreach (Collider2D collider in nearbyColliders)
+        {
+            if (givenGameObject == collider.gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Collider2D[] collidersWithinRange()
+    {
+        Vector2 centerPoint = DetermineReachableArea(reach);
+
+        float xReach = reach;
+        float yReach = reach;
+        if (dir == Dir.up || dir == Dir.down)
+        {
+            yReach /= 2;
+            xReach /= 1.33f;
+        }
+        else
+        {
+            yReach /= 1.33f;
+            xReach /= 2;
+        }
+
+        return Physics2D.OverlapBoxAll(centerPoint, new Vector2(xReach, yReach), 0f);
+        /*
         foreach (Collider2D collider in collidersAttack)
         {
             if (collider.tag == "Enemy")
@@ -106,12 +236,13 @@ public class PlayerMovement : AbstractMovement
                 GetComponent<GenericCreature>().DealDamage(collider.gameObject);
             }
         }
+        */
     }
 
-    private Vector2 DetermineSwingArea()
+    private Vector2 DetermineReachableArea(float boxSize)
     {
         Vector2 point = new Vector2();
-        float halfSwingSize = swingSize / 2;
+        float halfSwingSize = boxSize / 2;
         if (dir == Dir.up)
         {
             point = new Vector2(
@@ -145,5 +276,11 @@ public class PlayerMovement : AbstractMovement
             transform.position.y + point.y
         );
         return point;
+    }
+
+    public void ReachFor()
+    {
+        Animator handAnim = transform.GetChild(1).GetComponent<Animator>();
+        handAnim.SetTrigger("Reach");
     }
 }
