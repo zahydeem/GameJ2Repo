29d279668a49dc1ae0b6a273e3d138d07de32cc3
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-27 14:48:04 -0600

    Push

---
+++ gameJam/Assets/Scripts/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    private float range = 1f;
+
+    private void Update()
+    {
+        playerWithinRange();
+    }
+
+    private void OnMouseUp()
+    {
+        if (playerWithinRange())
+        {
+
+        }
+    }
+
+    public bool playerWithinRange()
+    {
+        return Vector2.Distance(GameController.gameController.player.transform.position, transform.position) < range;
+    }
+
+    protected abstract void Action();
+
+}
