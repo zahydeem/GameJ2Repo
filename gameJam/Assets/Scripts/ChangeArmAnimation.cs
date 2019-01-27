184298be7db0323d552b0df407b0db5e9cb2b3d7
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 23:57:13 -0600

    ohijo

---
+++ gameJam/Assets/Scripts/ChangeArmAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeArmAnimation : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("Movement", 4);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetInteger("Movement", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetInteger("Movement", 2);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetInteger("Movement", 3);
+        }
+        else
+        {
+            anim.SetInteger("Movement", 0);
+        }
+    }
+}
