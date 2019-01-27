7325a72b3905a07f90020f50c6f10c60bd5a9577
zahydeem <44513031+zahydeem@users.noreply.github.com>
2019-01-27 13:00:39 -0600

    notes

--- gameJam/Assets/Scripts/GameController.cs
+++ gameJam/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -9,12 +10,16 @@ public class GameController : MonoBehaviour
     public GameObject player;
 
     public float toolBarSize;
+    Canvas note;
+    private Text text; 
 
     // Start is called before the first frame update
     void Awake()
     {
         gameController = this;
         player = transform.GetChild(0).gameObject;
+        note = GetComponent<Canvas>();
+        text = GetComponent<Text>();
         
     }
 
@@ -29,6 +34,13 @@ public class GameController : MonoBehaviour
                 if (SelectObject().tag == "Interactable")
                 {
                     player.GetComponent<PlayerMovement>().ReachFor();
+                    print("jar");
+
+                }
+                else if (SelectObject().tag == "Note")
+                {
+                    print( "if");
+                    text.text = "Yay";
                 }
                 else
                 {
