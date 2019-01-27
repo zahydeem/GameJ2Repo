f252f911317c89b802ee6ebddf733d167eac9d30
Kaden Kilburg <kmkilburg28@gmail.com>
2019-01-26 18:51:47 -0600

    Update EnemyCritter.cs

--- gameJam/Assets/Scripts/EnemyCritter.cs
+++ gameJam/Assets/Scripts/EnemyCritter.cs
@@ -4,49 +4,47 @@ using UnityEngine;
 
 public class EnemyCritter : EnemyMovement
 {
-    float waitTime = 2000f;
+    float waitTime = 2f;
     float attackSpeed = 8f;
 
     float attackTime = 1000f;
+    float waitTimeStart;
 
     IEnumerator thisCoroutine;
 
     override protected void Attack()
     {
-    }
-        /*
         thisCoroutine = WaitRoutine();
         StartCoroutine(thisCoroutine);
 
-        loopStartedTime = Time.time;
-        Collider2D thisCollider = GetComponent<Collider2D>();
-        Collider2D playerCollider = player.GetComponent<Collider2D>();
-        while (Time.time - loopStartedTime < attackTime)
-        {
-            Move(attackSpeed);
-            if (thisCollider.IsTouching(playerCollider))
-            {
-                GetComponent<GenericCreature>().DealDamage(player);
-                break;
-            }
-        }
-
+        waitTimeStart = Time.time;
+        thisCoroutine = AttackRoutine();
+        StartCoroutine(thisCoroutine);
     }
 
 
 
     IEnumerator WaitRoutine()
     {
-        while (isInside)
+        yield break new WaitForSeconds(waitTime);
+        StopCoroutine(thisCoroutine);
+        yield return null;
+    }
+    IEnumerator AttackRoutine()
+    {
+        Collider2D thisCollider = GetComponent<Collider2D>();
+        while (Time.timeScale - waitTimeStart < attackTime)
         {
-            GManager.numBones--;
-            yield return new WaitForSeconds(1);
-        }
-        if (!isInside)
-        {
-            StopCoroutine(thisCoroutine);
+            Collider2D playerCollider = player.GetComponent<Collider2D>();
+            Move(attackSpeed);
+            if (thisCollider.IsTouching(playerCollider))
+            {
+                GetComponent<GenericCreature>().DealDamage(player);
+            }
             yield return null;
         }
+        StopCoroutine(thisCoroutine);
+        yield return null;
     }
-    */
+
 }
